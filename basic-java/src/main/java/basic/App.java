e81e99e667b42d5300d720708110c71e461adddd
liujinyao <liujinyao@chinatelecom.cn>
2019-07-20 18:20:34 +0800

    remove .iml

---
+++ basic-java/src/main/java/basic/App.java
@@ -0,0 +1,32 @@
+package basic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    public static void main( String[] args ) {
+        new Thread(new Runnable(){
+            @Override
+            public void run() {
+                System.out.println();
+            }
+        });
+
+        String str = null;
+        if(str.equals("a")) {
+            System.out.println();
+        }
+
+        String aVeryVeryWord = "dffjkdjkjskjkjkjkfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff";
+    }
+
+    public void hello() {
+
+    }
+
+    public void hh() {
+
+    }
+}
